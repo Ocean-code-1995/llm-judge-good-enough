dd8e19f6522945c3a0f634fab89e0cc2aee2c7d8
Sebastian Wefers <sebastianwefersnz@gmail.com>
2025-06-19 13:40:51 +0200

    introduced dir for reusable class

---
+++ llm_good_enough/evaluator.py
@@ -0,0 +1,27 @@
+
+
+class LLMGoodEnough:
+    def __init__(self):
+        """
+        """
+        pass
+
+    def compute_human_disagreements(self):
+        """
+        """
+        pass
+
+    def compute_llm_human_disagreements(self):
+        """
+        """
+        pass
+
+    def mannwhitney_test(self):
+        """
+        """
+        pass
+
+    def visulize_good_enough(self):
+        """
+        """
+        pass
